7c110461a9dd43cb9c159b25d9f882ff75bed118
root <ray.leizhu@outlook.com>
2024-01-31 23:28:17 +0800

    a100-40GB

--- _scripts/noninteractive_bench_sharegpt.sh
+++ _scripts/noninteractive_bench_sharegpt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREFIX_LENs=( 64 128 256 512 1024 2048 )
+PREFIX_LENs=( 512 1024 2048 64 128 256 )
 BACKENDs=( vllm+ )
 NUM_REQS=1000
 MODELs=( meta-llama/Llama-2-7b-hf meta-llama/Llama-2-13b-hf )
@@ -14,15 +14,18 @@ NOW=$(date "+%Y-%m-%d-%H.%M.%S")
 for MODEL in ${MODELs[@]}; do
     for PREFIX_LEN in ${PREFIX_LENs[@]}; do
         for BACKEND in ${BACKENDs[@]}; do
-            model_id=$(echo "$MODEL" | tr '/' '.')
+            # model_id=$(echo "$MODEL" | tr '/' '.')
+            model_id=$( basename $MODEL )
             # echo $model_id
             OUTPUT_DIR=outputs/noninteractive_bench_sharegpt/${GPU}/${model_id}/nreqs_${NUM_REQS}.prefixlen_${PREFIX_LEN}.backend_vllm+pc
+            # echo $OUTPUT_DIR
             mkdir -p $OUTPUT_DIR
             export TOKENIZERS_PARALLELISM=true && \
             python benchmarks/benchmark_throughput.py \
                 --backend $BACKEND \
                 --dataset $DATA_JSON\
                 --model $MODEL \
+                --load-format dummy \
                 --prefix-len $PREFIX_LEN \
                 --num-prompts $NUM_REQS \
                 --output-dir $OUTPUT_DIR \
--- _scripts/noninteractive_bench_synthetic.sh
+++ _scripts/noninteractive_bench_synthetic.sh
@@ -3,14 +3,14 @@
 # set -x
 
 # work load config
-IOs=( "64,128" "128,256" "256,256" )
+IOs=( "64,128" "128,256" "256,512" )
 # IOs=( "256,512" )
 PREFIX_LENs=( 512 1024 2048 256 128 64 )
 # PREFIX_LENs=( 1024 )
 NUM_REQS=1000
 
 # llama2 30B config
-MODEL=meta-llama/Llama-2-13b-hf
+MODEL=meta-llama/Llama-2-7b-hf
 
 GPU=$( nvidia-smi --query-gpu=name --format=csv | tail -n1 | tr ' ' '-' )
 NOW=$(date "+%Y-%m-%d-%H.%M.%S")
@@ -21,7 +21,8 @@ for IO in ${IOs[@]}; do
     # echo $CONTEXT_LEN $OUTPUT_LEN
     for PREFIX_LEN in ${PREFIX_LENs[@]}; do
         # model_id=$(echo "$MODEL" | tr '/' '.')
-        model_id=${MODEL#*/}
+        # model_id=${MODEL#*/}
+        model_id=$( basename $MODEL )
         # echo $model_id
         OUTPUT_DIR=outputs/noninteractive_bench_synthetic/${GPU}/${model_id}/nreqs_${NUM_REQS}.ctxlen_${CONTEXT_LEN}.outlen_${OUTPUT_LEN}.prefixlen_${PREFIX_LEN}.backend_vllm+pc
         mkdir -p $OUTPUT_DIR
@@ -32,6 +33,7 @@ for IO in ${IOs[@]}; do
             --output-dir $OUTPUT_DIR \
             --prefix-len $PREFIX_LEN \
             --model $MODEL \
+            --load-format dummy \
             --backend vllm+ \
             2>&1 | tee -a $OUTPUT_DIR/${NOW}.log
         sleep 1
