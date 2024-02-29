46a1ce0fe199f3698fd8ee8866063a018b4a5140
ZHU Lei <ray.leizhu@outlook.com>
2024-03-01 00:47:30 +0800

    Update install.sh

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,4 +1,6 @@
 
 conda create -n vllm python=3.9 -y && conda activate vllm
-conda install nvidia/label/cuda-12.1.1::cuda-toolkit
+conda install nvidia/label/cuda-12.1.1::cuda-toolkit 
+
+pip install -v -e .
 
